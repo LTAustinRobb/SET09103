d278d68fe8035d95c442cfbae5651cac68f6c06b
ltaustinrobb <ltaustinrobb@gmail.com>
2017-09-22 12:10:42 +0000

    new commit

--- hello.py
+++ hello.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, abort
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+@app.route('/force404')
+def force404():
+  abort(404)
+
+@app.errorhandler(404)
+def page_not_found(error):
+  return "This is not the page you're looking for", 404
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
 
 
---
+++ redirects.py
@@ -0,0 +1,17 @@
+
+from flask import Flask, redirect, url_for
+app = Flask(__name__)
+
+@app.route("/private")
+def private():
+  #Test for user login in failed
+  #redirect to login
+  return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+  return "Now we get username and password"
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
+
